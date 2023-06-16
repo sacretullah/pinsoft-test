b9392c2d99102a37eeaf936d0c506dd896367c6c
sacretullah <100204868+sacretullah@users.noreply.github.com>
2023-06-16 21:10:41 +0300

    son

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,19 +1,46 @@
 const initialState = {
-  filteredProductList: [],
-  cartItems: [],
+  cart: [],
 };
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "SET_FILTERED_LIST":
-      return {
-        ...state,
-        filteredProductList: action.payload,
-      };
     case "ADD_TO_CART":
+      const existingItem = state.cart.find(
+        (item) => item.id === action.payload.id
+      );
+      if (existingItem) {
+        const updatedCart = state.cart.map((item) => {
+          if (item.id === action.payload.id) {
+            return {
+              ...item,
+              count: item.count + 1,
+            };
+          }
+          return item;
+        });
+        return {
+          ...state,
+          cart: updatedCart,
+        };
+      } else {
+        const newItem = {
+          id: action.payload.id,
+          name: action.payload.title,
+          count: 1,
+          price: action.payload.price,
+        };
+        return {
+          ...state,
+          cart: [...state.cart, newItem],
+        };
+      }
+    case "REMOVE_FROM_CART":
+      const updatedCart = state.cart.filter(
+        (item) => item.id !== action.payload
+      );
       return {
         ...state,
-        cartItems: [...state.cartItems, action.payload],
+        cart: updatedCart,
       };
     default:
       return state;
